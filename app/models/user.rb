4a46a2b4cc3813dad13af4485973bfba4ab263f4
Ben Neely <benjaminneely@u.boisestate.edu>
2013-12-23 15:36:09 -0700

    Associated users and posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-  # attr_accessible :title, :body
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name
+  has_many :posts
+
 end
