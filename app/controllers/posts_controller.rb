9bb05c895d1058275b83e12b9ae6bda116708c0c
Ben Neely <benjaminneely@u.boisestate.edu>
2014-01-26 13:38:43 -0700

    Installed and implemented Redcarpet for Markdown.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,30 +15,31 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     authorize! :edit, @post, message: "You need to own the post to edit it."
   end
-
+  
   def create
     @topic = Topic.find(params[:topic_id])
     @post = current_user.posts.build(params[:post])
-    authorize! :create, @post, message: "You need to be signed up to do that."
+    @post.topic = @topic
+    
     if @post.save
       flash[:notice] = "Post was saved."
-      redirect_to @post
+      redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving the post. Please try again."
       render :new
     end
   end
-
+  
   def update
     @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
     authorize! :update, @post, message: "You need to own the post to edit it."
     if @post.update_attributes(params[:post])
       flash[:notice] = "Post was updated."
-      redirect_to @post
+      redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving the post. Please try again."
-      render :edit
+      render :new
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Bloccit::Application.routes.draw do
-  devise_for :users
 
   resources :topics do
     resources :posts, except: [:index]
   end
 
+  devise_for :users
+  
   match "about" => 'welcome#about', via: :get
 
   root :to => 'welcome#index'
